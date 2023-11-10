d2387da652022708060f1b724029ce6f2727296f
Kmilo169 <101821517+Kmilo169@users.noreply.github.com>
2023-11-10 01:59:54 +0100

    Quiz css pagina web example
    se sube los archivos correspondientes a la practica de front

---
+++ src/views/HomeView.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="fa">
+      +3115656296 babytracker@gmail.com
+    </div>
+
+    <div class="hom1">
+    
+    <nav>
+      <img class="Logou" alt="Bebe" src="../assets/bebe.png"> |
+      <router-link class="link" to="/about">Home</router-link> |
+      <router-link class="link" to="/about">About</router-link> |
+      <router-link class="link" to="/about">Clickme</router-link>
+    </nav>
+    </div>
+
+    <div>
+      <div class="todo">
+        <img class="fondo1" alt="Bebe1" src="../assets/bta.jpg">
+        <img class="fondo2" alt="Bebe2" src="../assets/ofi.jpg">
+      </div>
+      <div class="text1">
+        <h1>HOLA MUNDO</h1>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+</script>
+
+<style>
+
+.hom1{
+  background-color: black;
+  margin: auto;
+  position: top;
+  text-align: center;
+}
+
+.fa{
+  background-color: greenyellow;
+}
+
+.Logou {
+  width: 30px;
+  height: 30px;
+}
+
+.fondo1{
+  width: 50%;
+  height: 50%;  
+}
+
+.fondo2{
+  width: 50%;
+  height: 50%;
+}
+
+.text1{
+  position: relative;
+
+}
+.link{
+  color: whitesmoke;
+}
+.todo{
+  position: absolute;
+}
+
+
+</style>
